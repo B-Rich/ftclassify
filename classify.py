85f691ea4131eebd145bc79db9019ea314d40428
allan oware <lumtegis@gmail.com>
2012-06-22 01:29:15 +0300

    added getJenksBreaks method

--- classify.py
+++ classify.py
@@ -65,6 +65,7 @@ class Classify(webapp.RequestHandler):
 		classno = cgi.escape(self.request.get('classno'))
 		classno_ = int(classno)
 		classmethod = cgi.escape(self.request.get('classmethod'))
+		classmethod_ = int(classmethod)
 		
 		dList = []
 				
@@ -89,18 +90,81 @@ class Classify(webapp.RequestHandler):
 			interval = (maxValue - minValue)/classno
 			lower = minValue
 			
-			classes = []
+			classes = [lower]
 		
 			for i in range(classno):
 				upper = lower + interval
-				classes.append([lower,upper])
+				classes.append(upper)
 				lower = upper
 				
 				
 			return classes
 		
+		def getJenksBreaks(dataList, numClass):
+			dataList.sort()
+			mat1 = []
+			for i in range(0,len(dataList)+1):
+				temp = []
+				for j in range(0,numClass+1):
+					temp.append(0)
+				mat1.append(temp)
+			mat2 = []
+			for i in range(0, len(dataList)+1):
+				temp = []
+				for j in range(0,len(dataList)+1):
+					temp.append(0)
+				mat2.append(temp)
+			for i in range(1, numClass+1):
+				mat1[1][i] = 1
+				mat2[1][i] = 0
+				for j in range(2,len(dataList)+1):
+					mat2[j][i] = float('inf')
+			v = 0.0
+			for l in range(2,len(dataList)+1):
+				s1 = 0.0
+				s2 = 0.0
+				w = 0.0
+				for m in range(1, l+1):
+					i3 = l - m + 1
+					val = float(dataList[i3-1])
+					s2 += val * val
+					s1 += val
+					w += 1
+					v = s2 - (s1 * s1) / w
+					i4 = i3 - 1
+					if i4 != 0:
+						for j in range(2,numClass+1):
+							if mat2[l][j] >= (v + mat2[i4][j - 1]):
+								mat1[l][j] = i3
+								mat2[l][j] = v + mat2[i4][j - 1]
+				mat1[l][1] = 1
+				mat2[l][1] = v
+			k = len(dataList)
+			kclass = []
+			for i in range(0,numClass+1):
+				kclass.append(0)
+			kclass[numClass] = float(dataList[len(dataList) - 1])
+			countNum = numClass
+			while countNum >= 2:#print "rank = " str(mat1[k][countNum])
+				id = int((mat1[k][countNum]) - 2)
+				
+				kclass[countNum - 1] = dataList[id]
+				k = int((mat1[k][countNum] - 1))
+				countNum -= 1
+			return kclass
+				
+					
+		if classmethod_ == 1:
+			cList = getEqualInterval(dList, classno_)
+		elif classmethod_ == 2:
+			cList = getJenksBreaks(dList, classno_)
+		elif classmethod_ == 3:
+			cList = getJenksBreaks(dList, classno_)
+		else:
+			cList = getEqualInterval(dList, classno_)
+			
 		
-		cList = getEqualInterval(dList, classno_)
+		
 		title = 'Fusion Tables Data Classify Wizard'
 
 		page_values = {
